c79124e81e72cef33db3adcd625360cfe4a6d151
farhan <famannat@gmail.com>
2023-02-03 10:59:41 +0530

    find movies added

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 3000
 
 app.use(express.json())
 
 require('./db/conn')
 const Movies = require('./models/schema')
 
-app.post('/',async(req,res)=>{
+app.post('/movies',async(req,res)=>{
     const movies = new Movies(req.body)
     const Newmovies = await movies.save()
     console.log(Newmovies)
 })
 
 app.get('/movies',async(req,res)=>{
-    const moviesData = await Movies.find()
+    const FoundMovies = await Movies.find()
+    res.send(FoundMovies)
+    console.log(FoundMovies)
+})
+
+app.get('/movies/:id',async(req,res)=>{
+    const _id =  req.params.id
+    const moviesData = await Movies.findById(_id)
+    res.send(moviesData)
     console.log(moviesData)
 })
 
